ad8625ab143e1137c3d00bb9a6b3b2e71b5bad62
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2019-02-03 01:30:07 +0100

    Add a basic DB test and add test coverage monitoring  (#11)
    * Use tmp_path instead of depreciated tmpdir
    
    * Update comments on Member model definition
    
    * Add ability to test database
    
    * Fix import for database testing
    
    * Fix OTHER import for database testing
    
    * Install oauthlib and pytest-cov, and update backend dependencies
    
    * Enable coveralls for backend
    
    * Enable coverage generation
    
    * Try adding __init__.py to tests to try to fix coverage collection
    
    * fix tests imports
    
    * Fix coverage file path

---
+++ back/tests/test_db.py
@@ -0,0 +1,7 @@
+from tests import dummy_entries
+from tmeit_backend import models
+
+
+def test_get_name(dummy_database):
+    with dummy_database.app_context():
+        assert models.Member.query.get(dummy_entries.TEST_EMAIL).first_name == dummy_entries.TEST_FIRST_NAME
