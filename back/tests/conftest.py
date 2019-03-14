61bc8bc96a3eaaf5032a59f4d5be9784e83e155b
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2019-03-14 04:33:52 +0100

    Use an autogenerated database during testing

--- back/tests/conftest.py
+++ back/tests/conftest.py
@@ -1,14 +1,62 @@
 import pytest
+import os
 
-from tmeit_backend import models,api_app
+from tmeit_backend import models, api_app
+from tests import dummy_entries
+
 
 @pytest.fixture
-def app():
-    app = api_app.create_app('sqlite://', debug=True, testing= True)
-    models.db.create_all(app=app)
+def app_nodb():
+    """
+        Creates an app session for testing, using an empty db in memory.
+        app = api_app.create_app('sqlite:///{}/testing.sqlite3'.format(tmp_path), debug=True, testing=True)
+    """
+    app = api_app.create_app('sqlite://', debug=True, testing=True)
+
+    with app.app_context():
+        models.db.create_all()
+
     return app
 
-@pytest.fixture
-def app_with_testingdb():
-    app = api_app.create_app('sqlite:///testing.sqlite3', debug=True, testing=True)
+
+
+@pytest.fixture(scope="session")
+def app(tmp_path_factory):
+    """
+    Creates an app session for testing, and creates and uses a new database file with a test workteam and user.
+    """
+    tmp_path = tmp_path_factory.mktemp("tmeit-db")
+    app = api_app.create_app('sqlite:///{}/testing.sqlite3'.format(tmp_path), debug=True, testing=True)
+
+    with app.app_context():
+        models.db.create_all()
+
+    test_team = models.Workteam(
+        name=dummy_entries.TEST_TEAM_NAME,
+        symbol=dummy_entries.TEST_SYMBOL,
+        active=dummy_entries.TEST_ACTIVE,
+        active_year=dummy_entries.TEST_ACTIVE_YEAR,
+        active_period=dummy_entries.TEST_ACTIVE_PERIOD
+    )
+
+    test_user = models.Member(
+        email=dummy_entries.TEST_EMAIL,
+        first_name=dummy_entries.TEST_FIRST_NAME,
+        nickname=dummy_entries.TEST_NICKNAME,
+        last_name=dummy_entries.TEST_LAST_NAME,
+        phone=dummy_entries.TEST_PHONE,
+        drivers_license=dummy_entries.TEST_DRIVERS_LICENSE,
+        stad=dummy_entries.TEST_STAD,
+        fest=dummy_entries.TEST_FEST,
+        liquor_permit=dummy_entries.TEST_LIQUOR_PERMIT,
+        current_role=dummy_entries.TEST_CURRENT_ROLE,
+        workteams=[test_team],
+        workteams_leading=[test_team]
+    )
+
+    with app.app_context():
+        models.db.session.add(test_team)
+        models.db.session.add(test_user)
+        models.db.session.commit()
+
     return app
--- back/tests/test_auth.py
+++ back/tests/test_auth.py
@@ -9,19 +9,18 @@ from tmeit_backend import api_app, auth, models
 
 EXPIRED_GOOGLE_TOKEN = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjZmYjA1Zjc0MjM2NmVlNGNmNGJjZjQ5Zjk4NGM0ODdlNDVjOGM4M2QiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJhY2NvdW50cy5nb29nbGUuY29tIiwiYXpwIjoiNDk3MTA3NTAwNzA1LW5nc21paXFpM3A2cjFsNXBwMGdwZmduZnFmN2I4amNiLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwiYXVkIjoiNDk3MTA3NTAwNzA1LW5nc21paXFpM3A2cjFsNXBwMGdwZmduZnFmN2I4amNiLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTA5NzcwOTQ5ODY5ODk4NjkyMjgxIiwiZW1haWwiOiJ0ZXN0dG1laXRAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImF0X2hhc2giOiJjT0tMeHZtMjNRQUlQM2xZQ3FQOXRBIiwibmFtZSI6IlRlc3QgVE1FSVQiLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tLy1PVkdGY2NBUk1LZy9BQUFBQUFBQUFBSS9BQUFBQUFBQUFBQS9BQ2V2b1FOam5zcjNhcjZiVWVZZHI2cnJJTjZSRG1YMUFBL3M5Ni1jL3Bob3RvLmpwZyIsImdpdmVuX25hbWUiOiJUZXN0IiwiZmFtaWx5X25hbWUiOiJUTUVJVCIsImxvY2FsZSI6ImVuIiwiaWF0IjoxNTQ5Mjk0NDQ4LCJleHAiOjE1NDkyOTgwNDgsImp0aSI6ImY4NjBjYjAxYjZjMDE3OGVjMzZlNzVhYmM1YjAzM2E1OTJlNDBjZGMifQ.CuK1JkSmF4IOhNOq_fFhG_Y5AD6BHsNCnBjgQjE80oAajcpMFS2_VpPORzi8NrW8cgExyTd1zqs63LcaaFR1NPIu6mTA4b4YWYRdw9uhkD2em1cXAlCaRlyX4nFh_pUSNmi_q1ANOoyxaDkQo2-nr43DoZUM8gSTs2bXDZVaIrrE7Lf1JYFfw9xyGNYlrmzkzAGm9cLxddwEE93kZF3_6waF0MYVDUnLtzYw4rVPz-3DBNQGZRwnySY9QRv-ZCmUBV6rmZaQZSPAmO5bkXf9CTrBW7baIQsnaD5DoxxL1kQL1OBmMVTDvPCc6aoR-YdyFibRi5j-dEFr5cdasGExHg"
 
+
 class TestSecret:
     """Tests that secrets are being used and their security is verified."""
 
     def test_no_secret(self, tmp_path):
         """Tests that we're checking for a valid secret when loading in production"""
-
         os.chdir(tmp_path)
         with pytest.raises(FileNotFoundError):
             api_app.create_app('sqlite://', debug=False, testing=False)
 
     def test_weak_secret(self, tmp_path):
         """Tests that we're checking for a strong secret when loading in production"""
-
         os.chdir(tmp_path)
         with open('jwt_secret.txt', 'w') as secret_file:
             secret_file.write("too short secret not secure")
@@ -46,28 +45,31 @@ class TestLogin(object):
         assert r.status_code == 401
         assert r.json['msg'] == 'This Google JWT is invalid.'
 
-    def test_unregistered_user(self, client, monkeypatch):
+    def test_unregistered_user(self, app_nodb, monkeypatch):
         """Tests that we reject unregistered users"""
         # Patch over token verification
         monkeypatch.setattr(auth, 'verify_google_token', TestLogin.mock_external_token_verification)
+        app_nodb.config['SERVER_NAME'] = 'localhost:5000'
+        client = app_nodb.test_client()
         payload = {'access_token': EXPIRED_GOOGLE_TOKEN}
-        r = client.post(flask.url_for('login_page.login'), json=payload)
+        with app_nodb.app_context():
+            r = client.post(flask.url_for('login_page.login'), json=payload)
         assert r.status_code == 403
         assert r.json['msg'] == "Test Tmeit (testtmeit@gmail.com) is not registered"
 
-    def test_jwt_generation(self, app_with_testingdb, monkeypatch):
+    def test_jwt_generation(self, app, monkeypatch):
         """Tests that we're able to generate valid jwt tokens"""
         # Patch over token verification
         monkeypatch.setattr(auth, 'verify_google_token', TestLogin.mock_external_token_verification)
-        app_with_testingdb.config['SERVER_NAME'] = 'localhost:5000'
-        client = app_with_testingdb.test_client()
+        app.config['SERVER_NAME'] = 'localhost:5000'
+        client = app.test_client()
         payload = {'access_token': EXPIRED_GOOGLE_TOKEN}
-        with app_with_testingdb.app_context():
+        with app.app_context():
             r = client.post(flask.url_for('login_page.login'), json=payload)
         assert r.status_code == 200
-        config = app_with_testingdb.config
+        config = app.config
         token = jwt.decode(r.json['access_token'], key=config['JWT_SECRET_KEY'],
-                   issuer=config['JWT_ISSUER'], algorithms=config['JWT_ALGORITHM'])
+                           issuer=config['JWT_ISSUER'], algorithms=config['JWT_ALGORITHM'])
         assert token['sub'] == 'testtmeit@gmail.com'
 
     @staticmethod
