fa7c9f96489b0caf6647fe39f030a441c58b9844
wajd <wajdjtohme@gmail.com>
2019-04-26 22:28:06 +0200

    added profile

--- front/src/app/layouts/Team.js
+++ front/src/app/layouts/Team.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, {Component, Fragment} from "react";
+import {BrowserRouter as Router, Route, Link, Switch} from "react-router-dom";
+import Profile from "../components/Profile";
 
-function Team() {
-    return <h1>The Team</h1>;
-}
+function Team() {return (
+    <Router>
+        <Fragment>
+            <h1>Team</h1>
+            <h2><Link to="/team/5">User</Link></h2>
+            <h2><Link to="/team/6">User</Link></h2>
+            <h2><Link to="/team/8">User</Link></h2>
+            <Switch>
+                <Route exact path="/team/" />
+                <Route path="/team/:id" component={Profile}/>
+            </Switch>
+        </Fragment>
+    </Router>
+)}
 
 export default Team
