77bed2d65f34ab5c74fc7aa8a0605085805ae3b9
wajd <wajdjtohme@gmail.com>
2019-04-29 09:28:36 +0200

    index css + faux user obj in profile

--- front/src/app/components/Profile.js
+++ front/src/app/components/Profile.js
@@ -1,6 +1,21 @@
 import React from "react";
 import styles from "./profile.css"
 
+const user = {
+    id: "",
+    email: "",
+    profilePic: "",
+    name: "",
+    nickname: "",
+    gallery: "",
+    title: "",
+    team: "",
+    points: "",
+    eventsWorked: "",
+    eventsAttended: "",
+    permits: "",
+}
+
 function Profile ({match}) {
     return (
         <h2>Profile {match.params.id} </h2>
