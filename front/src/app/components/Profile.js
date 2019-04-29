6a168f0b105a171f1f1ce372ec379966a6ec9a3e
wajd <wajdjtohme@gmail.com>
2019-04-29 09:06:28 +0200

    minor fix in Team+Profile

--- front/src/app/components/Profile.js
+++ front/src/app/components/Profile.js
@@ -1,18 +1,9 @@
 import React from "react";
 import styles from "./profile.css"
 
-function Profile ({props}) {
+function Profile () {
     return (
-        <div className="container">
-            <div className="picture">
-                <img src="https://thispersondoesnotexist.com/image" alt="John Doe" />        
-            </div>
-            <div className="text">
-                <h1>Ovvenamn</h1>
-                <h2>Jonatan Hindén</h2>
-                <p>xx</p>
-            </div>
-        </div>
+        <h2>Profile</h2>
     )
 }
 
