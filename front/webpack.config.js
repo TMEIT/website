3fbadf91eb5ea24b50066c24c5ba350d831519db
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2019-05-01 00:21:44 +0200

    Make webpack-dev-server proxy api requests

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -35,6 +35,9 @@ module.exports = {
     }
 ,
     devServer: {
-        contentBase: './www'
+        contentBase: './www',
+        proxy: {
+            '/api': 'http://127.0.0.1:5000'
+        }
     }
 };
