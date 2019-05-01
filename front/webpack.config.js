66465711f5a68cd081ad529680d9b17872507d3e
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2019-05-01 02:01:38 +0200

    Remove babel env polyfill preset and update dev deps

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
                 use: { //more loaders can be added here in an array
                     loader: 'babel-loader',
                     options: {
-                        presets: ['@babel/preset-react', '@babel/preset-env']
+                        presets: ['@babel/preset-react']
                     }
                 }
             },
