f9e2efdc47195a37513ed21f3103898d3232287f
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2019-05-05 03:59:30 +0200

    Handle invalid profile links

--- front/src/app/layouts/Profile.js
+++ front/src/app/layouts/Profile.js
@@ -2,9 +2,20 @@ import React, {Fragment} from "react";
 import styles from "./profile.css"
 import {useFetch} from "../FetchHooks.js"
 import Loading from "../components/Loading";
+import {Redirect, Link} from "react-router-dom";
 
 function Profile ({match}) {
+
+    if(!match.params.id) return(<Redirect to={"/team"} />);
+
     const {loading, data} = useFetch("/api/members/" + match.params.id);
+    console.log(data);
+    if(data === "error") return(
+        <>
+            <h1>404: This profile was not found :(</h1>
+            Click <Link to={"/team"}>here</Link> to see all of our members.
+        </>
+    );
 
     return (
        <Fragment>
