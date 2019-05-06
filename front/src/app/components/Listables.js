87d98d6c05e89f4bdcc077898be934718d59d405
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2019-05-06 22:36:32 +0200

    Create WorkteamItem component for listing workteams

---
+++ front/src/app/components/Listables.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import styles from "./listables.css"
+
+
+export function WorkteamItem(props) {
+    const team = props.team;
+    return (
+        <div>
+            {team.name}
+        </div>
+    )
+}
+
+//Titles
+
+//Events
+
+//Members
