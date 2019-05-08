cc40549ec80cf90c54fd8f56d75bdf857a1cb8f3
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2019-05-08 22:14:13 +0200

    Delete InvalidExternaltokenError as it is no longer used

--- back/tmeit_backend/auth/errors.py
+++
@@ -1,7 +0,0 @@
-# auth.errors.py
-# Custom errors to use when handling authentication
-
-
-class InvalidExternalTokenError(RuntimeError):
-    """Raised when a user tries to login with a external token that is invalid."""
-    pass
