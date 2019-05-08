3c5952c240166517772c0c37bcdd75bc368763fe
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2019-05-08 08:38:45 +0200

    Add error handling to Team component

--- front/src/app/layouts/Team.js
+++ front/src/app/layouts/Team.js
@@ -7,18 +7,31 @@ function Team() {
 
     const {loading, data} = useFetch("/api/members");
 
+
+    let memberList;
+
+    //Loading
+    if(loading) memberList = <Loading />;
+
+    //API error
+    else if(data === "error") memberList = "Could not load API.";
+
+    // render memberlist
+    else {
+        memberList = data.objects.map(member =>
+                <h2 key={member.email} >
+                    <Link to={"/profile/" + encodeURIComponent(member.email)} >
+                        {member.first_name + " " + member.last_name}
+                    </Link>
+                </h2>
+            )
+    }
+
+
     return (
         <Fragment>
             <h1>Team</h1>
-            {loading? <Loading /> : 
-                data.objects.map(member =>
-                    <h2 key={member.email} >
-                        <Link to={"/profile/" + encodeURIComponent(member.email)} >
-                            {member.first_name + " " + member.last_name}
-                        </Link>
-                    </h2>
-                )
-            }
+            {memberList}
         </Fragment>
     )
 }
