865217ddc15fb839c4b4edee3b7ff26ed2177917
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2019-08-07 17:43:22 +0200

    Header: Port to styled-components styling

--- front/src/app/components/Header.js
+++ front/src/app/components/Header.js
@@ -1,23 +1,32 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
 
-import styles from "./header.css"
+const HeaderBox = styled.div`
+    background-color: #c5c5c5;
+    height: 100%;
+`;
+
+const Navigation = styled.nav`
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+`;
+
 
 
 function Header() {
     return (
-        <header>
-            <nav>
-                <ul>
-                    <li><NavLink to="/" activeClassName="selected">TMEIT</NavLink></li>
-                    <li><NavLink to="/events" activeClassName="selected">Events</NavLink></li>
-                    <li><NavLink to="/team" activeClassName="selected">Team</NavLink></li>
-                    <li><NavLink to="/join_tmeit" activeClassName="selected">JOIN</NavLink></li>
-                    <li><a href={"#"}>Login</a></li>
-                </ul>
-            </nav>
-
-        </header>
+        <HeaderBox className={props.className()}>
+            <Navigation>
+                <NavLink to="/" activeClassName="selected">TMEIT</NavLink>
+                <NavLink to="/events" activeClassName="selected">Events</NavLink>
+                <NavLink to="/team" activeClassName="selected">Team</NavLink>
+                <NavLink to="/join_tmeit" activeClassName="selected">JOIN</NavLink>
+                <a href={"#"}>Login</a>
+            </Navigation>
+        </HeaderBox>
     )
 }
 
