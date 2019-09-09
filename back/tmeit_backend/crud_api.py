f53f5668c6fc19652ab653e0bb8fe39f9b2ee55d
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2019-09-09 12:03:42 +0800

    Update comments

--- back/tmeit_backend/crud_api.py
+++ back/tmeit_backend/crud_api.py
@@ -5,6 +5,11 @@ import flask_restless
 
 from tmeit_backend import models
 
+def pre_put_member(*args, **kwargs):
+    # authentication if needed
+    # pre-filled fields
+    pass
+
 
 def add_crud_routes(app):
     manager = flask_restless.APIManager(app, flask_sqlalchemy_db=models.db)
