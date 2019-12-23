07a21e49573d6c643e95838cbf152de012c6cf62
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2019-12-23 07:53:31 +0100

    Backend: Use a fresh app for every test

--- back/tests/conftest.py
+++ back/tests/conftest.py
@@ -2,8 +2,8 @@ import pytest
 
 from tmeit_backend import models, main, dummy_entries
 
-@pytest.fixture(scope="session")
-def app(tmp_path_factory):
+@pytest.fixture(scope="function")
+def app():
     """
     Creates an app session for testing, and creates and uses a new database file with a test workteam and user.
     """
