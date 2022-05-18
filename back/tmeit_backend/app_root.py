d4b06ce34fc9abbc708bd6b789d04b3bea9df9b4
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2022-05-18 19:40:27 +0200

    Cleanup root app

--- back/tmeit_backend/app_root.py
+++ back/tmeit_backend/app_root.py
@@ -1,20 +1,27 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
+from starlette.routing import Mount
 
 from . import app_api
 
-# Root FastAPI app that routes incoming requests between static frontend files and backend API endpoints
-app = FastAPI()
+# This creates the root FastAPI app
+# This app routes incoming requests between static frontend files and backend API endpoints
 
-# API routes
-app.mount("/api/v1", app_api.app)
+
+routes = [
+    Mount("/api/v1", app_api.app),  # API routes
+    Mount("/static/front",  # Expose js and css from React frontend under /static/front
+          StaticFiles(directory="static/front"),
+          name="static-front"),
+]
+
+
+app = FastAPI(routes=routes)
+
 
 # Load SPA from frontend for "/" and "/*" endpoints https://stackoverflow.com/a/65917164
 @app.get("/")
 @app.get("/{page}")
-async def read_index():
+async def load_js_app(page=None):
     return FileResponse('static/front/index.html')
-
-# Expose js and css from frontend under /static/front
-app.mount("/static/front", StaticFiles(directory="static/front"), name="static-front")
