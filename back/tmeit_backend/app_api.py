a21480d0e393fdd3debf950608b2cf6f78c35e28
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2022-05-18 22:34:57 +0200

    Add basic get /member/{guid} endpoint

--- back/tmeit_backend/app_api.py
+++ back/tmeit_backend/app_api.py
@@ -1,9 +1,23 @@
+from uuid import UUID
+
 from fastapi import FastAPI
+
+from .api_models.members.model import Member
+from .api_models.members.enums import CurrentRoleEnum
 
 # Our FastAPI sub-app for the v1 API
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/members/{guid}", response_model=Member)
+def get_member(guid: UUID):
+    return Member(
+        guid=guid,
+        email="lmao@lol.se",
+        first_name="lm",
+        last_name="ao",
+        current_role=CurrentRoleEnum.master,
+        role_histories=[],
+        workteams=[],
+        workteams_leading=[],
+    )
