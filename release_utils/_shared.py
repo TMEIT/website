d3f32159898b360e221440d05e37b38c27f8fbbe
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2022-05-19 21:22:13 +0200

    Implement release-version checker script

---
+++ release_utils/_shared.py
@@ -0,0 +1,110 @@
+import configparser
+import json
+import subprocess
+from dataclasses import dataclass
+
+@dataclass
+class SemVer:
+    """A representation of a semantic version string"""
+    major: int
+    minor: int
+    patch: int
+
+    def __init__(self, semver_str: str):
+        """Creates a representation of a semantic version string. Only works on strings of the form "xx.yy.zz"."""
+        semver_str = semver_str.strip('"')  # Strip quotes inside string
+        self.major, self.minor, self.patch = [int(num) for num in semver_str.split(".")]
+
+    # convert back to string
+    def __str__(self):
+        return f'{self.major}.{self.minor}.{self.patch}'
+
+    # magic comparison functions
+    def __eq__(self, other):
+        return self.major == other.major \
+               and self.minor == other.minor \
+               and self.patch == other.patch
+
+    def __gt__(self, other):
+        # How to compare semver:
+        # https://stackoverflow.com/a/55466348
+        if self.major != other.major:
+            return self.major > other.major
+        elif self.minor != other.minor:
+            return self.minor > other.minor
+        else:
+            return self.patch > other.patch
+
+    def __le__(self, other):
+        return (not self == other) and (not self > other)
+
+
+@dataclass
+class FrontVersion:
+    package_json_path: str
+
+    @property
+    def version(self) -> SemVer:
+        with open(self.package_json_path) as front_package_file:
+            package = json.load(front_package_file)
+        front_ver_str: str = package["version"]
+        return SemVer(front_ver_str)
+
+    @version.setter
+    def version(self, new_version: SemVer) -> None:
+        with open(self.package_json_path, 'w') as front_package_file:
+            package = json.load(front_package_file)
+            package['version'] = str(new_version)
+            json.dump(package, front_package_file)
+
+
+@dataclass
+class BackVersion:
+    pyproject_toml_path: str
+
+    @property
+    def version(self) -> SemVer:
+        project = configparser.ConfigParser()
+        project.read(self.pyproject_toml_path)
+        back_ver_str: str = project['tool.poetry']['version']
+        return SemVer(back_ver_str)
+
+    @version.setter
+    def version(self, new_version: SemVer) -> None:
+        project = configparser.ConfigParser()
+        project.read(self.pyproject_toml_path)
+        project['tool.poetry']['version'] = str(new_version)
+        with open(self.pyproject_toml_path, 'w') as back_project_file:
+            project.write(back_project_file)
+
+
+@dataclass
+class K8sDeploymentImageTag:
+    tag_patch_path: str
+
+    @property
+    def version(self) -> SemVer:
+        with open(self.tag_patch_path) as image_tag_patch:
+            patch = json.load(image_tag_patch)
+        image_str: str = patch[0]["value"]
+        image, tag = image_str.split(":")
+        return SemVer(tag)
+
+    @version.setter
+    def version(self, new_version: SemVer) -> None:
+        with open(self.tag_patch_path, 'w') as image_tag_patch:
+            patch = json.load(image_tag_patch)
+            image_str: str = patch[0]["value"]
+            image, tag = image_str.split(":")
+            new_image_str = image + str(new_version)
+            patch[0]["value"] = new_image_str
+            json.dump(patch, image_tag_patch)
+
+
+@dataclass
+class LatestGitTag:
+
+    @property
+    def version(self) -> SemVer:
+        latest_tag: bytes = subprocess.run(["git", "describe", "--tags", "--abbrev=0"], capture_output=True).stdout
+        return SemVer(latest_tag.decode("utf-8"))
---
+++ release_utils/check-tag-incremented.py
@@ -0,0 +1,24 @@
+import json
+from dataclasses import dataclass
+
+from _shared import SemVer, FrontVersion, BackVersion, K8sDeploymentImageTag, LatestGitTag
+
+
+if __name__ == '__main__':
+    front_ver: SemVer = FrontVersion("front/package.json").version
+    back_ver: SemVer = BackVersion("back/pyproject.toml").version
+    k8s_image_tag: SemVer = K8sDeploymentImageTag("deploy/prod/set_image_tag.json").version
+
+    # Check if all versions are the same
+    if k8s_image_tag != front_ver or k8s_image_tag != back_ver:
+        raise RuntimeError(f"Versions do not match! ({front_ver=}, {back_ver=}, {k8s_image_tag=})")
+    else:
+        print("Versions match.")
+
+    # Check that versions have been incremented since last release
+    last_release: SemVer = LatestGitTag().version
+    if k8s_image_tag == last_release or k8s_image_tag < last_release:
+        raise RuntimeError("Versions have not been incremented! "
+                           f"Last version: {last_release}, Current version: {k8s_image_tag}")
+    else:
+        print("Versions have been incremented, ready to release.")
