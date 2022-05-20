17c52876685cd19c2e1f203f0b9354f4ec20ded1
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2022-05-20 16:23:19 +0200

    Add a push and release pipeline

---
+++ release_utils/get_version.py
@@ -0,0 +1,5 @@
+from release_utils._shared import SemVer, K8sDeploymentImageTag
+
+if __name__ == '__main__':
+    k8s_image_tag: SemVer = K8sDeploymentImageTag("deploy/tmeit-jlh-name/set_image_tag.json").version
+    print(str(k8s_image_tag))
