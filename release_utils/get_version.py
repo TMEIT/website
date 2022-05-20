6f739c14540581d98eda00581d3174c61c95d0e5
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2022-05-20 16:50:53 +0200

    Fix how pipeline gets version number

--- release_utils/get_version.py
+++ release_utils/get_version.py
@@ -1,5 +1,12 @@
+import sys
+
 from _shared import SemVer, K8sDeploymentImageTag
 
 if __name__ == '__main__':
     k8s_image_tag: SemVer = K8sDeploymentImageTag("deploy/tmeit-jlh-name/set_image_tag.json").version
-    print(str(k8s_image_tag))
+
+    option: str = sys.argv[-1]
+    if option == "gh-actions":  # Store version as a variable for github actions when "gh-actions" is given as an arg
+        print("::set-output name=version::" + str(k8s_image_tag))
+    else:
+        print(str(k8s_image_tag))
