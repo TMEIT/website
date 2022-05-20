8ba4c33dad9fa22248304ea055beea0b486f6ac7
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2022-05-20 16:41:21 +0200

    Fix version script for pipeline (#76)

--- release_utils/get_version.py
+++ release_utils/get_version.py
@@ -1,4 +1,4 @@
-from release_utils._shared import SemVer, K8sDeploymentImageTag
+from _shared import SemVer, K8sDeploymentImageTag
 
 if __name__ == '__main__':
     k8s_image_tag: SemVer = K8sDeploymentImageTag("deploy/tmeit-jlh-name/set_image_tag.json").version
