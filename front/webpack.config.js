093741212b7ee69bae7199ba9d077d1cfa7306f9
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2022-05-25 09:31:42 +0200

    Fix mobile scaling

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -47,6 +47,10 @@ module.exports = {
             templateContent: `
               <!DOCTYPE html>
               <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=0.86, maximum-scale=5.0, minimum-scale=0.86">
+                </head>
                 <body>
                   <div id="root"></div>
                 </body>
