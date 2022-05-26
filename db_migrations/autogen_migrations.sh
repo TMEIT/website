22d6e45526f33de239b9dec01deb6dbd5dccb6ca
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2022-05-26 19:24:21 +0200

    Set up alembic and add a migration autogeneration script

---
+++ db_migrations/autogen_migrations.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This generates migrations for the database
+# You must have your tilt environment running for this to work
+# You also need poetry installed on your machine, and the backend dependencies installed to your current virtualenv
+
+# Password in url must be same as deploy/dev/kustomization.yaml
+export SQLALCHEMY_DATABASE_URL='postgresql+asyncpg://tmeit_backend:HBXOHEc6TpkquVHKy2zmSeUIEaUFvW@localhost:5432/tmeit_backend'
+
+# Port forward to db while script is running
+trap 'kill $(jobs -p)' EXIT;
+kubectl port-forward svc/tmeit-db 5432 > /dev/null &
+
+echo "Enter a short comment about what you're changing in this migration"
+read -r COMMENT
+
+VENV_PATH=$(. cd back && poetry env info --path)
+"$VENV_PATH"/bin/alembic -c db_migrations/alembic.ini revision --autogenerate -m "$COMMENT"
