002c9e630d3b20adf686e7a03b614ee96e67bf34
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2022-05-29 23:24:59 +0200

    Add new version tag release logic for migrations job

--- release_utils/check-tag-incremented.py
+++ release_utils/check-tag-incremented.py
@@ -8,10 +8,14 @@ if __name__ == '__main__':
     front_ver: SemVer = FrontVersion("front/package.json").version
     back_ver: SemVer = BackVersion("back/pyproject.toml").version
     k8s_image_tag: SemVer = K8sDeploymentImageTag("deploy/tmeit-jlh-name/tmeit-app/set_image_tag.json").version
+    k8s_m_job_image_tag: SemVer = \
+        K8sDeploymentImageTag("deploy/tmeit-jlh-name/run-migrations/set_image_tag.json").version
 
     # Check if all versions are the same
-    if k8s_image_tag != front_ver or k8s_image_tag != back_ver:
-        raise RuntimeError(f"Versions do not match! ({front_ver=}, {back_ver=}, {k8s_image_tag=})")
+    if k8s_image_tag != front_ver or k8s_image_tag != back_ver or k8s_image_tag != k8s_m_job_image_tag:
+        raise RuntimeError(
+            f"Versions do not match! ({front_ver=}, {back_ver=}, {k8s_image_tag=}, {k8s_m_job_image_tag=})"
+        )
     else:
         print("Versions match.")
 
