8380d3a047b5e07813ac8cf176fd7467ee0b72b7
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2022-05-29 21:35:13 +0200

    Fix migrations test 2

--- back/db_migrations/scripts/pr-migration-tests.sh
+++ back/db_migrations/scripts/pr-migration-tests.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-export POSTGRES_HOSTNAME=localhost
+export POSTGRES_HOSTNAME=postgres
 export POSTGRES_PORT=5432
 export POSTGRES_PASSWORD=test
 
 # Run create_test_database to run all migrations with dummy data
-/code/.venv/bin/python scripts/create_test_database.py check-missing-migrations
+/code/.venv/bin/python scripts/create_test_database.py
 
 # Check if someone forgot to generate migrations
 /code/.venv/bin/alembic-autogen-check
