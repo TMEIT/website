aa003bfec74e0a2ccd8a6397a8332a146a2e9a1a
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2022-05-30 00:25:12 +0200

    Add missing networkpolicy for migrations job

--- back/db_migrations/scripts/pr-migration-tests.sh
+++ back/db_migrations/scripts/pr-migration-tests.sh
@@ -5,7 +5,9 @@ export POSTGRES_PORT=5432
 export POSTGRES_PASSWORD=test
 
 # Run create_test_database to run all migrations with dummy data
+echo Running create_test_database.py
 /code/.venv/bin/python scripts/create_test_database.py
 
 # Check if someone forgot to generate migrations
+echo Checking for missing migrations
 /code/.venv/bin/alembic-autogen-check
