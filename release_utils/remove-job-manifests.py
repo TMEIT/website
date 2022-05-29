a6235cfb6329159be03c41b29ffc77e9b64f4217
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2022-05-30 01:39:19 +0200

    Dont lint k8s jobs 3

--- release_utils/remove-job-manifests.py
+++ release_utils/remove-job-manifests.py
@@ -3,16 +3,17 @@ import re
 # Jobs are immutable and if we do kubectl apply with one without deleting it on the cluster, the api will throw an error
 
 if __name__ == '__main__':
-    with open("jlh-rendered.yaml", "r+") as f:
-        text = f.read()
-        f.seek(0)
-        manifests = re.split("---\n", text)  # Break file into individual manifests
+    with open("jlh-rendered-with-jobs.yaml", "r") as of:
+        text = of.read()
+
+    manifests = re.split("---\n", text)  # Break file into individual manifests
+
+    with open("jlh-rendered.yaml", "w") as f:
         for manifest in manifests:
-            # Write manifests back to file but skip any Jobs
+            # Write manifests to new file but skip any Jobs
             if manifest.startswith("apiVersion: batch/v1\nkind: Job"):
                 print("Deleting Job manifest")
             else:
                 manifest_with_separator = manifest + "---\n"
                 print(manifest_with_separator, end='')
                 f.write(manifest_with_separator)
-        f.truncate()  # https://stackoverflow.com/a/2424410
