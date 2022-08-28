8eb55e88807813e9ced527dd81043362c94aec73
axelfors <axelforshallen@gmail.com>
2022-08-28 23:44:29 +0200

    Sign up done with need for redierct site routing

---
+++ front/src/app/components/Joined.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Joined() {
+  return (
+    <div>
+      <h1>
+        You are now registered to TMEIT! You now have to wait for the masters to
+        approve your registration before it will be visible
+      </h1>
+      <a href="/">
+        <h2>Click here to go back to the home page</h2>
+      </a>
+    </div>
+  );
+}
+
+export default Joined;
