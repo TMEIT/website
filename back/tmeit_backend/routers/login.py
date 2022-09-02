03eb4fa3d0fdf2d6e47e864d590f762903245779
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2022-09-02 10:57:21 +0200

    Fix docs for /token

--- back/tmeit_backend/routers/login.py
+++ back/tmeit_backend/routers/login.py
@@ -1,6 +1,7 @@
 import datetime
 import os
 import traceback
+from typing import Literal
 
 from fastapi import Depends, status, HTTPException, APIRouter
 from fastapi.security import OAuth2PasswordRequestForm
@@ -27,7 +28,11 @@ class Token(BaseModel):
     token_type: str
 
 
-@router.post("/token", response_model=Token)
+class InvalidLoginResponse(BaseModel):
+    detail: Literal["Incorrect email or password"]
+
+
+@router.post("/token", response_model=Token, responses={401: {"model": InvalidLoginResponse}})
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(),
                                  db: AsyncSession = Depends(get_db)):
     try:
@@ -36,7 +41,7 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
     except Exception:
         traceback.print_exc()
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                            detail="Incorrect username or password",
+                            detail="Incorrect email or password",
                             headers={"WWW-Authenticate": "Bearer"})
     access_token_expires = datetime.timedelta(days=ACCESS_TOKEN_EXPIRE_DAYS)
     access_token = jwt_authenticator.create_member_access_token(login_email=member.login_email,
