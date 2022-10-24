83da3db5c484a74a2a1b1a8bc4b53f9ba5c5fede
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2022-10-24 23:22:38 +0200

    Fully migrate site deploy to prod server

--- release_utils/remove-job-manifests.py
+++ release_utils/remove-job-manifests.py
@@ -3,12 +3,12 @@ import re
 # Jobs are immutable and if we do kubectl apply with one without deleting it on the cluster, the api will throw an error
 
 if __name__ == '__main__':
-    with open("jlh-rendered-with-jobs.yaml", "r") as of:
+    with open("tmeit-rendered-with-jobs.yaml", "r") as of:
         text = of.read()
 
     manifests = re.split("---\n", text)  # Break file into individual manifests
 
-    with open("jlh-rendered.yaml", "w") as f:
+    with open("tmeit-rendered.yaml", "w") as f:
         for manifest in manifests:
             # Write manifests to new file but skip any Jobs
             if manifest.startswith("apiVersion: batch/v1\nkind: Job"):
