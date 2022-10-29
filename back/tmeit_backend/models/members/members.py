84b47055e10db147b287abc6b2265105a0c47095
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2022-10-29 15:18:44 +0200

    Finish adding old_username field

--- back/tmeit_backend/models/members/members.py
+++ back/tmeit_backend/models/members/members.py
@@ -34,7 +34,7 @@ class Member(Base):
     short_uuid = Column(String, Computed(short_uuid_from_uuid(uuid)), unique=True, index=True)
 
     # If this user was migrated from the old tmeit.se, what their username was
-    old_username = Column(String, nullable=False, unique=True)
+    old_username = Column(String)
 
     # Created/Updated timestamps
     time_created = Column(DateTime(timezone=True), server_default=func.now())
