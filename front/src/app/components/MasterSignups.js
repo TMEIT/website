89ef4a269cf7a4ace738f36288d1ff9720e8ea71
axelfors <axelforshallen@gmail.com>
2022-10-30 19:28:16 +0100

    Working signup handling
    Out commented code is for re-rendering when signup is removed but could not get working

--- front/src/app/components/MasterSignups.js
+++ front/src/app/components/MasterSignups.js
@@ -1,20 +1,68 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import Loading from "../components/Loading";
 import { useFetch } from "../FetchHooks";
 import { useNavigate } from "react-router-dom";
 
 function MasterSignups() {
   const { loading, data } = useFetch("/api/v1/sign_up");
+  //const currentData  = useRef(second)
 
   let signups;
   let navigate = useNavigate();
 
-  function handleAdd(e) {
-    console.log(e);
-    var uuid = e.uuid;
+  function handleAdd(uuid, context) {
+    var tokenText = cookie();
+
+    const address = "/api/v1/sign_up/approve/" + uuid;
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", address, true);
+    xhr.setRequestHeader("Authorization", tokenText);
+    xhr.send("") 
+    xhr.responseType = "json";
+
+    xhr.onload = function () {
+      if(xhr.status >= 200 && xhr.status < 300){
+        //let currentData = context.filter(x => {return x.uuid != uuid;});
+        alert("User has been added");
+      }
+      else{
+        alert(`error: ${xhr.status}: ${xhr.statusText}`);
+      }
+    };
+    xhr.onerror = function (){
+      console.log("Request failed");
+    }
+  }
+
+  function handleDelete(uuid, context) {
+
+    var tokenText = cookie();
+    const address = "/api/v1/sign_up/" + uuid;
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("DELETE", address, true);
+    xhr.setRequestHeader("Authorization", tokenText);
+    xhr.responseType = "";
+    xhr.send();
+
+    xhr.onload = function () {
+      if(xhr.status >= 200 && xhr.status < 300){
+        //let currentData = context.filter(x => {return x.uuid != uuid;});
+        alert("User has been deleted");
+      }
+      else{
+        alert(`error: ${xhr.status}: ${xhr.statusText}`);
+      }
+    };
+    xhr.onerror = function (){
+      alert("Request failed");
+    }
+  }
+
+  function cookie(){
     var name = "access_token";
     var cookieArr = document.cookie.split(";");
-    var tokenText;
 
     //Loop through array until token is found
     for (var i = 0; i < cookieArr.length; i++) {
@@ -25,72 +73,48 @@ function MasterSignups() {
           // If value for access_token is empty
           navigate(0);
         } else {
-          tokenText = "Bearer " + decodeURIComponent(cookiePair[1]);
+          return "Bearer " + decodeURIComponent(cookiePair[1]);
         }
       }
     }
-
-    const xhr = new XMLHttpRequest();
-    xhr.open("POST", "api/v1/sign_up/approve/" + uuid);
-    xhr.setRequestHeader("Authorization", tokenText);
-    xhr.responseType = "json";
-
-    console.log("reached xhr");
-
-    xhr.onload = function () {
-      if (xhr.status >= 200 && xhr.status < 300) {
-        console.log("It works");
-      } else {
-        console.log("USER NOT ADDED, MESSAGE FROM SERVER: " + xhr.statusText);
-      }
-    };
-
-    console.log("after onload");
+    navigate(0);
   }
 
-  function handleDelete(e) {
-    console.log(e);
-    var uuid = e.uuid;
-    var name = "access_token";
-    var cookieArr = document.cookie.split(";");
-    var tokenText;
-
-    //Loop through array until token is found
-    for (var i = 0; i < cookieArr.length; i++) {
-      var cookiePair = cookieArr[i].split("=");
-
-      if (name === cookiePair[0].trim()) {
-        if (decodeURIComponent(cookiePair[1]) === "") {
-          // If value for access_token is empty
-          navigate(0);
-        } else {
-          tokenText = "Bearer " + decodeURIComponent(cookiePair[1]);
-        }
-      }
-    }
-
-    const xhr = new XMLHttpRequest();
-    xhr.open("DELETE", "api/v1/sign_up/" + uuid);
-    xhr.setRequestHeader("Authorization", tokenText);
-    xhr.responseType = "json";
-
-    xhr.onload = function () {
-      if (xhr.status === 200) {
-        if (this.status >= 200 && this.status < 300) {
-          alert("user was deleted");
-        }
-      } else {
-        alert("USER NOT DELETED, MESSAGE FROM SERVER: " + this.statusText);
-      }
-    };
-  }
+  // useEffect(() => {
+  //   console.log("useEffect triggered");
+  //   console.log(currentData);
+  //     signups = currentData.map((e) => (
+  //     <>
+  //       <p>{e.login_email}</p>
+  //       <p>
+  //         {e.first_name} {e.last_name}
+  //       </p>
+  //       <p>Phone: {e.phone}</p>
+  //       <p>
+  //         Signup submitted at {e.time_created} from ip address {e.ip_address}
+  //       </p>
+  //       <button onClick={() => handleAdd(e.uuid, currentData)}>Add to members</button>
+  //       <button onClick={() => handleDelete(e.uuid, currentData)}>Delete</button>
+  //     </>
+  //   ))}, [currentData])
 
   // Loading
   if (loading) signups = <Loading />;
   // API error
   else if (data === "error") signups = "Could not load API";
   else {
-    console.log(data);
+    // let currentData = data.map((e) => (
+    //   {
+    //     login_email: e.login_email,
+    //     first_name: e.first_name, 
+    //     last_name: e.last_name,
+    //     phone: e.phone,
+    //     time_created: e.time_created,
+    //     ip_address: e.ip_address,
+    //     uuid: e.uuid
+    //   }
+    // ));
+    
     signups = data.map((e) => (
       <>
         <p>{e.login_email}</p>
@@ -101,13 +125,18 @@ function MasterSignups() {
         <p>
           Signup submitted at {e.time_created} from ip address {e.ip_address}
         </p>
-        <button onClick={() => handleAdd(e)}>Add to members</button>
-        <button onClick={() => handleDelete(e)}>Delete</button>
+        <button onClick={() => handleAdd(e.uuid, currentData)}>Add to members</button>
+        <button onClick={() => handleDelete(e.uuid, currentData)}>Delete</button>
       </>
-    ));
+    ))
   }
 
-  return <Fragment>{signups}</Fragment>;
+  return (
+    <>
+      <Fragment>{signups}</Fragment>
+      {/* <button onClick={() => console.log(currentData)}>Cum</button> */}
+    </>
+  )
 }
 
 export default MasterSignups;
