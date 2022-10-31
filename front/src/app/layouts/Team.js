00a91b0a2ef3ebe8225af11dede60c0dfeeb9969
axelfors <axelforshallen@gmail.com>
2022-10-31 20:13:06 +0100

    Team now sorts based on current role

--- front/src/app/layouts/Team.js
+++ front/src/app/layouts/Team.js
@@ -7,32 +7,80 @@ function Team() {
 
     const {loading, data} = useFetch("/api/v1/members/");
 
-
-    let memberList;
+    let loadMembers = false;
+    let loadingElement;
+    let masterList, marshalList, praoList, exList, vraqList;
 
     //Loading
-    if(loading) memberList = <Loading />;
+    if(loading) loadingElement = <Loading />;
 
     //API error
-    else if(data === "error") memberList = "Could not load API.";
+    else if(data === "error") loadingElement = "Could not load API.";
 
     // render memberlist
     else {
-        memberList = data.map(member =>
-                <h2 key={member.uuid} >
+        loadMembers = true;
+        loadingElement = null;
+        masterList = data.filter(member => member.current_role === "master").map(member =>
+                <h3 key={member.uuid} >
                     <Link to={`/profile/${member.short_uuid}/${member.first_name}_${member.last_name}` } >
                         {member.first_name + " " + member.last_name}
                     </Link>
-                </h2>
+                </h3>
             )
+        marshalList = data.filter(member => member.current_role === "marshal").map(member =>
+            <h3 key={member.uuid} >
+                <Link to={`/profile/${member.short_uuid}/${member.first_name}_${member.last_name}` } >
+                    {member.first_name + " " + member.last_name}
+                </Link>
+            </h3>
+        )
+        praoList = data.filter(member => member.current_role === "prao").map(member =>
+            <h3 key={member.uuid} >
+                <Link to={`/profile/${member.short_uuid}/${member.first_name}_${member.last_name}` } >
+                    {member.first_name + " " + member.last_name}
+                </Link>
+            </h3>
+        )
+        vraqList = data.filter(member => member.current_role === "vraq").map(member =>
+            <h3 key={member.uuid} >
+                <Link to={`/profile/${member.short_uuid}/${member.first_name}_${member.last_name}` } >
+                    {member.first_name + " " + member.last_name}
+                </Link>
+            </h3>
+        )
+
+        exList = data.filter(member => member.current_role === "ex").map(member =>
+            <h3 key={member.uuid} >
+                <Link to={`/profile/${member.short_uuid}/${member.first_name}_${member.last_name}` } >
+                    {member.first_name + " " + member.last_name}
+                </Link>
+            </h3>
+        )
+
     }
 
 
     return (
+        <>
+        {loadingElement}
+        {loadMembers ?
         <Fragment>
             <h1>Team</h1>
-            {memberList}
+            <h2>Masters</h2>            
+            {masterList}
+            <h2>Marshals</h2>
+            {marshalList}
+            <h2>Prao</h2>
+            {praoList}
+            <h2>Vraq</h2>
+            {vraqList}
+            <h2>Ex-members</h2>
+            {exList}
         </Fragment>
+        : <></>
+        }
+    </>
     )
 }
 
