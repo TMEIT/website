7cbe8c054cec387f8b37981c20ba313be32528e1
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2022-11-02 23:05:50 +0100

    Add send test email endpoint

--- back/tmeit_backend/worker_tasks/send_test_email.py
+++ back/tmeit_backend/worker_tasks/send_test_email.py
@@ -1,4 +1,5 @@
 
 
-async def send_test_email(ctx):
-    print("yeet")
+async def send_test_email(ctx, member_uuid):
+    """Sends a test email to a given member, as specified by the member's UUID"""
+    print(member_uuid)
