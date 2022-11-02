392979c3bae695888a8330e20f9352b7f91d8ff7
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2022-11-02 23:41:23 +0100

    Add database to tmeit-worker

--- back/tmeit_backend/redis.py
+++ back/tmeit_backend/redis.py
@@ -1,5 +1,12 @@
 from arq.connections import RedisSettings
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from typing import TypedDict, Callable, ContextManager
 
 # Redis connection setting for arq
 redis_settings = RedisSettings(host=[('rfs-redis', 26379)], sentinel=True, sentinel_master="mymaster")
+
+
+# Type definition for the worker_task context
+class WorkerContext(TypedDict):
+    db_session: Callable[..., ContextManager[AsyncSession]]
