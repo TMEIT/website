310025727fc9e6add1faa46042bea6654ad21ff5
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2022-11-02 23:41:23 +0100

    Add database to tmeit-worker

--- back/tmeit_backend/arq.py
+++ back/tmeit_backend/arq.py
@@ -1,17 +1,25 @@
+from .database import get_production_url, get_async_engine, get_async_session
 from .worker_tasks import functions
-from .redis import redis_settings
+from .redis import redis_settings, WorkerContext
 
 
 # This file is the entrypoint for the tmeit-worker container. This file is not used by the app.
+
+
+async def startup(ctx: WorkerContext):
+    db_url = get_production_url()
+    db_engine = get_async_engine(db_url, echo=True)
+    ctx['db_session'] = get_async_session(db_engine)
+
 
 class WorkerSettings:
     """
     Configuration used to start the worker container
 
-    functions is the list of functions that the worker can run (and the app can call)
-
     redis_settings defines how to connect to redis
-
+    on_startup runs on every startup and can store things in the ctx dictionary
+    functions is the list of functions that the worker can run (and the app can call)
     """
-    functions = functions
     redis_settings = redis_settings
+    on_startup = startup
+    functions = functions
--- back/tmeit_backend/worker_tasks/send_test_email.py
+++ back/tmeit_backend/worker_tasks/send_test_email.py
@@ -1,5 +1,15 @@
+from sqlalchemy import select
+
+from .. import models
+from ..redis import WorkerContext
 
 
-async def send_test_email(ctx, member_uuid):
+async def send_test_email(ctx: WorkerContext, member_uuid: str):
     """Sends a test email to a given member, as specified by the member's UUID"""
-    print(member_uuid)
+    async with ctx['db_session']() as db:
+        stmt = select(models.Member).where(models.Member.uuid == str(member_uuid))
+        result = (await db.execute(stmt)).fetchone()
+        if result is None:
+            raise KeyError()
+        member: models.Member = result.Member
+    print(member.login_email)
