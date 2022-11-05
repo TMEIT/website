d6ed7f433a73a3b1c0b5973659e7f228adb8ea7b
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2022-11-05 22:56:05 +0100

    Put events page into textsummary

--- front/src/app/layouts/Events.js
+++ front/src/app/layouts/Events.js
@@ -1,12 +1,15 @@
 import React from "react";
 
+import TextSummary from "../components/TextSummary.js";
+
 function Events() {
     return(
-        <>
+        <TextSummary>
+
             <h1>Events</h1>
             <p>Events page coming soon.</p>
-            <p>Check out our <a href="https://www.facebook.com/TMEIT/events">Facebook events</a> in the meantime!</p>
-        </>
+            <p>Check out our <a href="https://www.facebook.com/TMEIT/events" target="_blank">Facebook events</a> in the meantime!</p>
+        </TextSummary>
     );
 }
 export default Events
