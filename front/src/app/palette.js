ab52040df0a32c6ead9e19a4809f2078f43638dd
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2022-11-06 06:34:13 +0100

    Add new colors from chapter and colormind

--- front/src/app/palette.js
+++ front/src/app/palette.js
@@ -4,11 +4,26 @@
 export const kiesel_blue = "#44687d";
 export const kiesel_light_blue = "#8cb1c8";
 
+export const me_green = "#ADFF5C";
+export const data_pink = "#E83D84";
+export const laser_purp = "#CC99FF";
+export const qmisk_red = "#800000";
+
+
+export const me_and_in_teal = "#30BDAA"
+
+
 export const secondary_purp = "#696886";
 export const secondary_purp_dark = "#23243d";
 
 export const primary_light = "#b5aa9b";
+export const primary_lighter = "#ded2c3";
 
 export const accent_yellow = "#a1873f";
 
 export const accent_red = "#993623";
+
+// Accent hues taken from combining kiesel_blue, primary_lighter, and secondary_purp_dark in colormind with Pokemon Crystal preset
+export const accent_maroon = "#361B36";
+
+export const accent_salmon = "#EA8174";
