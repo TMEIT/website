9aacc550b32fc5df1bbb8c403690f343f3d56f1a
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2022-11-08 00:05:21 +0100

    More updates to the Join page

--- front/src/app/layouts/Join.js
+++ front/src/app/layouts/Join.js
@@ -3,7 +3,7 @@ import css from 'styled-jsx/css';
 
 import sjo_group from "../layout_photos/sjoslaget_group_2022.webp";
 
-import { kiesel_blue, secondary_purp, me_green, accent_maroon, accent_salmon } from "../palette.js";
+import { kiesel_blue, secondary_purp, secondary_purp_dark, me_green, data_pink, me_and_in_teal, primary_light, primary_lighter, accent_yellow, accent_maroon, accent_salmon } from "../palette.js";
 import JoinForm from "../components/JoinForm";
 import TextSummary from "../components/TextSummary.js";
 import Centered from "../components/Centered.js";
@@ -35,16 +35,43 @@ export const join_style = css`
         font-size: calc(0.6rem + 4vw);
         font-family: Cantarell, sans-serif;
         font-weight: 900;
-        border-radius: 0.5rem;
-        padding: 0.5rem;
-        padding-left: 1rem;
-        padding-right: 1rem;
-    }
-    .joining {
-        place-self: center;
-    }
-    .joining>div {
         padding: 1em;
+        padding-bottom: 0.5em;
+
+    }
+
+    .joining {
+        display: grid;
+    }
+    @media (max-width: 60rem) {
+        .joining {
+            grid: "about" auto "form" auto "gdpr" auto / 1fr;
+        }
+        .joining > div {
+            padding: 1em;
+        }
+    }
+    @media (min-width: 60rem) {
+        .joining {
+            grid: "about form gdpr" auto / 1fr 1fr 1fr;
+        }
+        .joining > div {
+            padding: 2em;
+        }
+    }
+
+    .aboutJoining {
+        grid-area: about;
+        background: ${me_and_in_teal};
+        font-size: 1.5em;
+    }
+    .form {
+        grid-area: form;
+        background: ${primary_lighter};
+    }
+    .gdpr {
+        grid-area: gdpr;
+            background: ${data_pink};
     }
     .group-photo {
         width: 100%;
@@ -61,8 +88,10 @@ function Join() {
     <div className="container">
         <h1> So you want to join TMEIT? </h1>
         <AboutTmeit />
+        <h1> Become PRAO! </h1>
         <div className="joining">
-            <TextSummary>
+            <div className="aboutJoining">
+                <h2> How to join TMEIT </h2>
                 <p>
                     We're always looking for new people to join our community, and the period to become a PRAO is open all year long.
                 </p>
@@ -70,6 +99,8 @@ function Join() {
                     Just sign up using the form, and you'll be invited to all of TMEIT's events!
                     We'll also create an account for you here on tmeit.se, so you can see everything that's happening in the klubbmästeri.
                 </p>
+                <br />
+                <h2> Once you've signed up </h2>
                 <p>
                     Make sure you check your email for the confirmation email we'll send you when your account is confirmed.
                 </p>
@@ -77,8 +108,38 @@ function Join() {
                     If you're joining us during Mottagning,
                     note that TMEIT holds a special meeting at the end of Mottagning with all new PRAO that you don't want to miss!
                 </p>
-            </TextSummary>
-            <JoinForm />;
+            </div>
+            <div className="form">
+                <JoinForm />;
+            </div>
+            <div className="gdpr">
+                <h2> Your personal data </h2>
+                <h3> How your personal data is used </h3>
+                <p>
+                    As a club organizing social activities for its members, TMEIT stores data about you to help plan these activities and facilitate social interaction with other members.
+                    Some of the data we store includes your name, your email address, your phone number, when your account was created,
+                    special events that happen during your membership in TMEIT, Workteams you participate in, events you attended, photos uploaded of you,
+                    and any other data that you or other members upload to the site.
+                </p>
+                <h3> Who can see your data </h3>
+                <p>
+                    As a member of TMEIT, your name, some photos of you, your current Workteam,
+                    and whether you have attended/are attending an event are displayed publicly on the website.
+                    Please contact one of the Masters if this is an issue.
+                    All other personal data can only be seen by other members of TMEIT.
+                </p>
+                <h3> Website logs </h3>
+                <p>
+                    To combat bots and spammers, we log page accesses and their associated IP addresses and store those logs for three months.
+                    When the PRAO form is filled out, we also log the IP address of the user who submitted the form.
+                    Only administrators can see these logs and IP addresses.
+                </p>
+                <h3> Data security, data controllers, and data processors </h3>
+                <p>
+                    All personal data is stored securely in the EU in Finnish and Dutch datacenters with our cloud providers Hetzner and Backblaze.
+                    If you have any questions or concerns about how we use your personal data, please contact one of the Masters.
+                </p>
+            </div>
         </div>
         <h1> See you at the pub! </h1>
         <img className="group-photo" src={sjo_group} alt="A group photo of TMEIT after Sjöslaget 2022" />
