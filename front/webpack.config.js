e1c3632039a0c80f713ffae916dd158b132d0f8d
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2022-11-08 03:45:00 +0100

    Fix font loading

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -38,7 +38,7 @@ module.exports = {
                 use: ['style-loader', 'css-loader']
             },
             {
-                test: /\.(woff(2)?|ttf|eot|svg|png|webp)?$/,
+                test: /\.(svg|png|webp)?$/,
                 use: {
                     loader: 'url-loader'
                 }
