82b9af10986bd4321a70baf247bb57c2cf8b8dfc
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2022-11-09 06:26:57 +0100

    Refactor out styled-jsx from Centered.js

--- front/src/app/components/Centered.js
+++ front/src/app/components/Centered.js
@@ -1,24 +1,20 @@
 import React from "react";
+import styled from "@emotion/styled";
 
-import css from 'styled-jsx/css'
+const StyledCentered = styled(Centered)({
+    display: "grid",
+    gridTemplateColumns: "1fr auto 1fr",
+    "& > .inner": { gridColumnStart: 2 }
+});
 
-export const center_a_div_style = css`
-    .outer {
-        display: grid;
-        grid-template-columns: 1fr auto 1fr;
-    }
-    .inner { grid-column-start: 2; }
-`
-
-function Centered({children}) {
+function Centered({className, children}) {
     return (
-        <div className="outer">
+        <div className={className}>
             <div className="inner">
                 {children}
             </div>
-            <style jsx> {center_a_div_style} </style>
         </div>
     );
 }
 
-export default Centered
+export default StyledCentered;
