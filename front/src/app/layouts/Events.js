85663dac973395b4d7e93c4985a03b440ce12da4
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2022-11-09 06:36:42 +0100

    Refactor out styled-jsx from Events.js

--- front/src/app/layouts/Events.js
+++ front/src/app/layouts/Events.js
@@ -1,29 +1,26 @@
 import React from "react";
-import css from 'styled-jsx/css'
+import styled from "@emotion/styled";
 
 import Centered from "../components/Centered.js";
 import TextSummary from "../components/TextSummary.js";
 
-const max_width_style = css`
-    div {
-        max-width: 80em;
-        margin: 2em;
+const StyledEvents = styled(Events)({
+    [TextSummary]: {
+        maxWidth: "80em",
+        margin: "2em"
     }
-`
+});
 
-function Events() {
+function Events({className}) {
     return(
-        <Centered>
-            <div>
-                <TextSummary>
-                    <h1>Events - Coming soon!</h1>
-                    <p>The events page is under construction.</p>
-                    <p>TMEIT.SE GEN 3 is very new and does not yet have the event pages that the old site had.</p>
-                    <p>Check out our <a href="https://www.facebook.com/TMEIT/events" target="_blank">Facebook events</a> in the meantime!</p>
-                </TextSummary>
-            </div>
-            <style jsx> {max_width_style} </style>
+        <Centered className={className}>
+            <TextSummary>
+                <h1>Events - Coming soon!</h1>
+                <p>The events page is under construction.</p>
+                <p>TMEIT.SE GEN 3 is very new and we're still re-implementing event pages feature.</p>
+                <p>Check out our <a href="https://www.facebook.com/TMEIT/events" target="_blank">Facebook events</a> in the meantime!</p>
+            </TextSummary>
         </Centered>
     );
 }
-export default Events
+export default StyledEvents
