e423b99810120ed3706efd3d86ab7b98efd8e317
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2022-11-09 18:25:36 +0100

    Set up tree shaking in webpack

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -85,13 +85,7 @@ module.exports = {
 
         })
     ],
-    devServer: {
-        contentBase: './www',
-        historyApiFallback: {
-            disableDotRule: true
-        },
-        proxy: {
-            '/api': 'http://127.0.0.1:5000'
-        }
-    }
+    optimization: {
+        usedExports: true,
+    },
 };
