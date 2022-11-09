52d58cd401c0d28cb7c492e2fd9b19a8627aaf37
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2022-11-09 06:36:32 +0100

    Refactor out styled-jsx from Centered.js

--- front/src/app/components/TextSummary.js
+++ front/src/app/components/TextSummary.js
@@ -1,26 +1,21 @@
 import React from "react";
-import {Link} from "react-router-dom";
-
-import css from 'styled-jsx/css'
+import styled from "@emotion/styled";
 
 import { kiesel_blue, secondary_purp, primary_light, primary_lighter } from "../palette.js";
 
 
-export const center_a_div_style = css`
-    .textbox {
-        padding: 1em;
-        background: ${primary_lighter};
-        border-radius: 1em;
-    }
-`
+const StyledTextSummary = styled(TextSummary)({
+    padding: "1em",
+    background: primary_lighter,
+    borderRadius: "1em",
+});
 
-function TextSummary({children}) {
+function TextSummary({className, children}) {
     return (
-        <div className="textbox">
+        <div className={className}>
             {children}
-            <style jsx> {center_a_div_style} </style>
         </div>
     );
 }
 
-export default TextSummary
+export default StyledTextSummary
