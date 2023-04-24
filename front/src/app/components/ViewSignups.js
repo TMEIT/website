5587e9bbea12f328b533146645f4f699a881f2eb
Sebbe <sdiv@kth.se>
2023-04-24 21:19:35 +0200

    Refactored the code for the view signups layouts, and components

--- front/src/app/components/ViewSignups.js
+++ front/src/app/components/ViewSignups.js
@@ -7,11 +7,7 @@ import SignupListItem from "../components/SignupListItem.js";
 import { kisel_blue, kisel_blue_dark, primary_lighter } from "../palette";
 import { getApiFetcher } from "../api.js";
 
-const StyledSignupList = styled(SignupList) ({ 
-    display: "grid",
-    justifyContent: "space-around",
-    alignItems: "center",
-})
+const StyledSignupList = styled(SignupList) ({});
 
 function SignupList({className, children}) {
     return (
@@ -25,7 +21,34 @@ function SignupList({className, children}) {
 const render_signups = (data, role) => 
     Object.values(data).filter(data => data.role === role).map(data => {console.log(data); return(<SignupListItem worker={data}/>)})
 
-const StyledViewSignups = styled(ViewSignups)({});
+const StyledViewSignups = styled(ViewSignups)({
+    h2: {
+        fontSize: "12px",
+    },
+
+    ".detailRow": {
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fit, minmax(1.2em, 1fr))",
+    },
+
+    ".detailBox": {
+        borderStyle: "solid",
+        borderColor: "black",
+        borderWidth: "thin",
+        overflow: "hidden",
+        textOverflow: "ellipsis",
+    },
+
+    "@media (max-width: 950px)" :{
+        h2: {
+            fontSize: "8px",
+        },
+
+        ".detailRow": {
+            gridTemplateColumns: "repeat(auto-fil, minmax(1.4em, 1fr))",
+        }
+    }
+});
 
 function ViewSignups({className, eventID})
 {
@@ -53,19 +76,30 @@ function ViewSignups({className, eventID})
     
     return(
         <div className={className}>
-            <Box sx={{display: "grid", marginTop: 2, marginBottom: 2, padding: "1em", borderRadius: "1em", bgcolor: primary_lighter}}>
-                <Grid>
-                    <h1>Signups for event: {eventID} </h1>
-                </Grid>
-                <Grid>
+            <Box sx={{display: "grid", gridTemplateRows: "repeat(auto-fit, minmax(20px, 1fr))", marginTop: 2, marginBottom: 2, padding: "1em", borderRadius: "1em", bgcolor: primary_lighter}}>
+                <div>
+                    <Grid>
+                        <h1>Signups for event: {eventID} </h1>
+                    </Grid>
+                </div>
+                <div className="detailRow">
+                    <Grid className="detailBox">  <h2>Name: </h2>                         </Grid>
+                    <Grid className="detailBox">  <h2>Nick: </h2>                         </Grid>
+                    <Grid className="detailBox">  <h2>Role: </h2>                         </Grid>
+                    <Grid className="detailBox">  <h2>Phone no.: </h2>                    </Grid>
+                    <Grid className="detailBox">  <h2>Can work?: </h2>                    </Grid>
+                    <Grid className="detailBox">  <h2>From: </h2>                         </Grid>
+                    <Grid className="detailBox">  <h2>To: </h2>                           </Grid>
+                    <Grid className="detailBox">  <h2>Will leave?: </h2>                  </Grid>
+                    <Grid className="detailBox">  <h2>From: </h2>                         </Grid>
+                    <Grid className="detailBox">  <h2>To: </h2>                           </Grid>
+                    <Grid className="detailBox">  <h2>Comment: </h2>                      </Grid>
+                </div>
+                <div>
                     <StyledSignupList>{masterList}</StyledSignupList>
-                </Grid>
-                <Grid>
                     <StyledSignupList>{marshalList}</StyledSignupList>
-                </Grid>
-                <Grid>
                     <StyledSignupList>{praoList}</StyledSignupList>
-                </Grid>
+                </div>
             </Box>
         </div>
     );
