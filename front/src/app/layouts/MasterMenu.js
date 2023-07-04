8866295b87fdf87507651bc43e21b9f5aef27c22
Sebastian Divander <sdivkth@gmail.com>
2023-07-04 22:36:21 +0200

    Redesigned the master menu - it looks a lot better already :P

--- front/src/app/layouts/MasterMenu.js
+++ front/src/app/layouts/MasterMenu.js
@@ -1,5 +1,9 @@
 import { useState } from "react";
 import {Link} from "react-router-dom";
+import {Button} from "@mui/material";
+import {Grid} from "@mui/material";
+import {Box} from "@mui/material";
+import Centered from "../components/Centered.js";
 
 import MasterSignups from "../components/MasterSignups";
 
@@ -8,21 +12,36 @@ function MasterMenu() {
   const [menu, setMenu] = useState(0);
   return (
     <>
-        <Link to="/migrating">See Pending member migrations</Link>
-      <div>
-        <button onClick={() => setMenu(1)}>Signups menu</button>
-      </div>
+      <Centered>
+        <Grid sx={{justifyContent: "space-around", display: "grid"}}>
+          <Box mt={3} mb={3} mx={"Auto"}>
+            <Button variant="contained"><Link to="/migrating">See Pending member migrations</Link></Button>
+          </Box>
+          <Box mt={3} mb={3} mx={"Auto"}>
+            <Button variant="contained" onClick={() => setMenu(2)}>Signups menu</Button>
+          </Box>
+          <Box mt={3} mb={3} mx={"Auto"}>
+            <Button variant="contained" onClick={() => setMenu(3)}>Create Event</Button>
+          </Box>
+          <Box mt={3} mb={3} mx={"Auto"}>
+            <Button variant="contained" onClick={() => setMenu(4)}>Edit/Delete Events</Button>
+          </Box>
+        </Grid>
+      </Centered>
       <div>
         {(() => {
           switch (menu) {
             case 0:
               return <></>;
             case 1:
-              return <MasterSignups></MasterSignups>;
-            case 2:
               return <></>;
-
+            case 2:
+              return <MasterSignups></MasterSignups>;
             case 3:
+              return <></>;
+            case 4:
+              return <></>;
+            case 5:
               return <></>;
           }
         })()}
