5550573030d29d3f3b47d0b795e2db36005ec312
Sebastian <sdivkth@gmail.com>
2023-07-07 12:40:43 +0200

    Made the master menu look even nicer, and optimized for mobile

--- front/src/app/layouts/MasterMenu.js
+++ front/src/app/layouts/MasterMenu.js
@@ -1,42 +1,45 @@
-import styled from "@emotion/styled"; 
 import { useState } from "react";
 import {Link} from "react-router-dom";
 import {Button} from "@mui/material";
-import {Grid} from "@mui/material";
 import {Box} from "@mui/material";
 import Centered from "../components/Centered.js";
 import useIsScreenWide from "../useIsScreenWide.js";
 
 import MasterSignups from "../components/MasterSignups";
 
-const StyledMasterMenu = styled(MasterMenu)({
-  ".button" : {
-      width : "16vw",
-
-  }
-});
-
 function MasterMenu() {
+
+  let screenWide = useIsScreenWide(949);
+
+  const styles = {
+    button :{
+      width: "16vw",
+      marginTop: "2em",
+      marginBottom: "2em",
+    },
+
+    buttonMobile:{
+      width: "30vw",
+      marginTop: "1em",
+      marginBottom: "1em",
+    }
+  };
+
   const isScreenWide = useIsScreenWide(950);
 
   const [menu, setMenu] = useState(0);
   return (
     <>
       <Centered>
-        <Grid sx={{justifyContent: "space-around", display: "grid"}}>
-          <Box mt={2} mb={2} mx={"Auto"}>
-            <Button className="button" variant="contained"><Link to="/migrating">See Pending member migrations</Link></Button>
-          </Box>
-          <Box mt={2} mb={2} mx={"Auto"}>
-            <Button className="button" variant="contained" onClick={() => setMenu(2)}>Signups menu</Button>
-          </Box>
-          <Box mt={2} mb={2} mx={"Auto"}>
-            <Button className="button" variant="contained" onClick={() => setMenu(3)}>Create Event</Button>
-          </Box>
-          <Box mt={2} mb={2} mx={"Auto"}>
-            <Button className="button" variant="contained" onClick={() => setMenu(4)}>Edit/Delete Events</Button>
-          </Box>
-        </Grid>
+        <Box mt={2} mb={2} mx={"Auto"} style={{display: "grid", gridAutoFlow: "row"}}>
+          <Button style={screenWide? styles.button : styles.buttonMobile} variant="contained"><Link to="/migrating">See Pending member migrations</Link></Button>
+        
+          <Button style={screenWide? styles.button : styles.buttonMobile} variant="contained" onClick={() => setMenu(2)}>Signups menu</Button>
+      
+          <Button style={screenWide? styles.button : styles.buttonMobile} variant="contained" onClick={() => setMenu(4)}>Edit/Delete Events - ADMIN</Button>
+
+          <Button hidden={menu==0} style={screenWide? styles.button : styles.buttonMobile} variant="contained" onClick={() => setMenu(0)}>Back</Button>
+        </Box>
       </Centered>
       <div>
         {(() => {
@@ -59,4 +62,4 @@ function MasterMenu() {
     </>
   );
 }
-export default StyledMasterMenu;
+export default MasterMenu;
