11f2a3cc56b24e4cc54467f50f36411e90d523da
Axel Karlsson <axel.v.m.karlsson@gmail.com>
2023-09-18 22:12:23 +0200

    bugfix and formatting of email

--- back/tmeit_backend/worker_tasks/send_password_reset_email.py
+++ back/tmeit_backend/worker_tasks/send_password_reset_email.py
@@ -23,8 +23,8 @@ async def send_password_reset_email(ctx: WorkerContext, email: str, reset_token:
         f'Hi {full_name},\n\n'
         'You are receiving this email since a pasword reset has been requested for your tmeit account.\n\n'
         'If you did not make this request, please ignore this message.\n\n'
-        'You can reset your password by visiting <a href=\"tmeit.se/reset/{reset_token}\">tmeit.se/reset/{reset_token}</a>\n\n'
-        "XOXO\n"
+        f'You can reset your password by visiting <a href=\"tmeit.se/reset/{reset_token}\">tmeit.se/reset/{reset_token}</a>\n\n'
+        "XOXO,\n"
         "A fucking computer\n"
     )
     
@@ -32,7 +32,7 @@ async def send_password_reset_email(ctx: WorkerContext, email: str, reset_token:
         sending_user="tmeit",
         to_display_name=full_name,
         to_email='1@test.com',#to_email=email,
-        subject=f"Password reset requested - TMEIT - {datetime.now(timezone.utc)}",
+        subject=f"Password reset requested - TMEIT - {str(datetime.now(timezone.utc))[0:16]}",
         message_text=body,
         message_html=(email_header + convert_body_to_html(body) + '</body></html>'),
     )
