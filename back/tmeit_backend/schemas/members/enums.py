683eb2590f7fe9ada70c546ff8857c18f07a1d83
Sebastian <sdivkth@gmail.com>
2024-01-07 14:31:34 +0100

    Added new roles for Webb, Gourmet and Junk

--- back/tmeit_backend/schemas/members/enums.py
+++ back/tmeit_backend/schemas/members/enums.py
@@ -10,5 +10,20 @@ class CurrentRoleEnum(str, Enum):
     ex = "ex"
     exprao = "exprao"
 
+    # Marshal Roles - WebbMarshal, GourmetMarshal, JunkMarshal
+    webbmarshal =       "WebbMarshal"
+    gourmetmarshal =    "GourmetMarshal"
+    junkmarshal =       "JunkMarshal"
+
+    # Prao Roles - WebbPrao, GourmetPrao, JunkPrao
+    webbprao =          "WebbPrao"
+    gourmetprao =       "GourmetPrao"
+    junkprao =          "JunkPrao"
+
+    # Vraq Roles - WebbVraq, GourmetVraq, JunkVraq
+    webbvraq =          "WebbVraq"
+    gourmetvraq =       "GourmetVraq"
+    junkvraq =          "JunkVraq"
+
     def is_active(self):
         return self.value != "exprao"
